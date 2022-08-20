a486373a8cb2059d379ada515632ec2fc6a10a2f
timeenjoyed <timeenjoyed@protonmail.com>
2022-08-20 16:57:25 -0700

    Set DEBUG based on environment variables

--- learning_log/learning_log/settings.py
+++ learning_log/learning_log/settings.py
@@ -133,6 +133,12 @@ LOGIN_URL = 'users:login'
 
 
 # Heroku settings
-# This function modifies some settings that need specific values for the Heroku environmenbt
+# This function modifies some settings that need specific values for the Heroku environment
+import os
 import django_heroku
 django_heroku.settings(locals())
+
+if os.environ.get('DEBUG') == 'TRUE':
+    DEBUG = True
+elif os.environ.get('DEBUG') == 'FALSE':
+    DEBUG = False
